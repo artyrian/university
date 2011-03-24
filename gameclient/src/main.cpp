0c2096e12352feaff2b4277a36bc66907224b3bc
artyrian <artyrian@ya.ru>
2011-03-24 23:28:59 +0300

    Move try block to parser

--- gameclient/src/main.cpp
+++ gameclient/src/main.cpp
@@ -1,22 +1,12 @@
 #include <stdio.h>
 #include "lexer/parser.hpp"
-#include "lexer/exeption.hpp"
 
 int main (int argc, char ** argv)
 {
 	if ( argc > 1 ) {
-		try {
-			Parser p (argv[1]);
+		Parser p (argv[1]);
 
-			 p.analyze (); 
-		}
-		catch (const LexExeption & le) {
-			printf ("catch exeption.\n");
-			le.print ();
-
-			return 1;
-
-		}
+		p.analyze (); 
 	}
 	else {
 		perror ("Not enough parameters.\n");
