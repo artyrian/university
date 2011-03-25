ce68c8915131cb34c2ece2d11c2486bda2216a4f
artyrian <artyrian@ya.ru>
2011-03-25 23:58:40 +0300

    Add path to exeption. and exeption now stay here

--- gameclient/src/main.cpp
+++ gameclient/src/main.cpp
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include "lexer/parser.hpp"
+#include "exeption/exeption.hpp"
 
 int main (int argc, char ** argv)
 {
 	if ( argc > 1 ) {
-		Parser p (argv[1]);
+		try {
+			Parser p (argv[1]);
 
-		p.analyze (); 
+			p.analyze (); 
+		}
+		catch (const LexExeption & le) {
+			printf ("catch exeption.\n");
+			le.print ();
+			return 1;
+		}
+		catch (int i) {
+			printf ("Exeption: %d.\n", i);
+			return 1;
+		}
+
 	}
 	else {
 		perror ("Not enough parameters.\n");
