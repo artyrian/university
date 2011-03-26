d88a9a83e7acbf23d1970a94ae8744e663d918a0
artyrian <artyrian@ya.ru>
2011-03-26 03:14:06 +0300

    Add some descriptions of Poliz elements.

--- gameclient/src/lexer/poliz.cpp
+++ gameclient/src/lexer/poliz.cpp
@@ -1,1 +1,91 @@
 #include "poliz.hpp"
+
+PolizElem:: ~PolizElem ()
+{
+}
+
+
+PolizInt (int a)
+{
+	value = a;
+}
+
+PolizInt:: ~PolizInt ()
+{
+}
+
+PolizElem * PolizInt:: clone () const
+{
+	return new PolizInt (value);
+}
+
+int PolizInt:: get () const
+{
+	return value;
+}
+
+PolizLabel:: PolizLabel ()
+{
+	value = a;
+}
+
+PolizLabel:: ~PolizLabel ()
+{
+}
+
+PolizElem * PolizLabel:: clone () const
+{
+	return new PolizLabel (value);
+}
+
+PolizElem * PolizLabel:: get () const
+{
+	return value;
+}
+
+
+
+PolizOpGo:: PolizOpGo ()
+{
+}
+
+PolizOpGo:: ~ PolizOpGo ()
+{
+}
+
+void PolizOpGo:: evaluate ( PolizItem ** stack,
+			    PolizItem ** cur_cmd)
+const
+{
+	PolizElem * operand1 = pop (stack);
+	PolizLabel * lab = dynamic_cast<PolizLabel *>(operand1);
+	if ( !lab ) throw PolizExNoLabel (operand1);
+	PolizItem * addr = lab->get ();
+	* cur_cmd = addr;
+	delete operand1;
+}
+
+/*
+PolizFunPlus:: PolizFunPlus ()
+{
+}
+
+PolizFunPlus:: ~PolizFunPlus ()
+{
+}
+
+PolizFunPlus:: evaluate_fun (PolizItem ** stack) const
+{
+	PolizElem * operand1 = pop (stack);
+	PolizLabel *i1 = dynamic_cast<PolizInt *>(operand1);
+	if ( ! i1 ) throw PolizExNotInt (operand1);
+}
+lizElem *operand2 = Pop(stack);
+	PolizLabel *i2 = dynamic_cast<PolizInt*>(operand2);
+	if(!i2) throw PolizExNotInt(operand2);
+	int res = i1->Get() + i2->Get();
+	delete operand1;
+	delete operand2;
+	return new PolizInt(res);
+}
+*/
--- gameclient/src/lexer/poliz.hpp
+++ gameclient/src/lexer/poliz.hpp
@@ -1,8 +1,64 @@
 #ifndef _POLIZ_HPP_
 #define _POLIZ_HPP_
 
+class PolizElem {
+public:
+	virtual ~PolizElem ();
+	virtual void evaluate (	PolizItem ** stack, 
+				PolizItem ** cur_cmd) const = 0;
+protected:
+	static void 		push (PolizItem ** stack, PolizElem * elem);
+	static PolizElem * 	pop (PolizItem ** stack);
+};
+
+class PolizConst /* */ {
+/*
+	Not ready yet.
+*/
+
+}
+
+class PolizInt : public PolizConst {
+	int value;
+public:
+	PolizInt (int a);
+	virtual ~PolizInt ();
+	virtual PolizElem * clone () const;
+	int get () const;
+};
+
+
+class PolizLabel : public PolizConst {
+	PolizItem * value;
+public:
+	PolizLabel (PolizItem * a);
+	virtual ~PolizLabel ();
+	virtual PolizElem * clone () const;
+	PolizItem * get () const;
+
+};
+
+class PolizOpGo : public PolizElem {
+public:
+	PolizOpGo ();
+	virtual ~PolizOpGo ();
+	void evaluate ( PolizItem ** stack,
+			PolizItem ** cur_cmd) const;
+};
+/*
+Here class PolizFunction 
+
+class PolizFunPlus : public PolizFunPlus {
+public:
+	PolizFunPlus ();
+	virtual ~PolizFunPlus ();
+	PolizElem * evaluate_fun ( PolizItem ** stack) const;
+};
+*/
+
 class Poliz {
 
 };
+
 
 #endif
